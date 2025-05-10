79ccf60261011f7f3168904dec8f77bd241e3450
Gigaulitka <none@example.com>
2025-05-10 23:14:24 +0300

    Use Libmicrohttpd for the metrics server
    See https://www.gnu.org/software/libmicrohttpd/.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS=-Wall -pedantic
 all: build test
 
 build:
-	$(CC) $(CFLAGS) -lpthread main.c -o ./rerun
+	$(CC) $(CFLAGS) main.c -o ./rerun -lmicrohttpd
 
 test:
 	$(foreach file, $(wildcard tests/*.exp), echo "Run tests from $(file):" && expect "$(file)" && echo || exit 1;)
--- main.c
+++ main.c
@@ -6,26 +6,17 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
-#include <pthread.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <poll.h>
-#include <fcntl.h>
 #define _GNU_SOURCE  // For getopt_long
 #include <getopt.h>
+
+#include <microhttpd.h>
 
 
 struct Metrics {
     int success_total;
     int failure_total;
-};
-
-
-struct MetricsThreadArgs {
-    const char *host;
-    int port;
-    struct Metrics *metrics;
-    int exit_pipe_fd;
 };
 
 
@@ -70,163 +61,62 @@ const char *usage_message =
     } while (0)
 
 
-void handle_http_request(int server_fd, struct sockaddr_in address, struct Metrics *metrics)
+enum MHD_Result handle_http_request(
+    void *cls,
+    struct MHD_Connection *connection,
+    const char *url,
+    const char *method,
+    const char *version,
+    const char *upload_data,
+    long unsigned int *upload_data_size,
+    void **con_cls
+)
 {
-    socklen_t addrlen = sizeof(address);
-    int client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
-    if (client_fd == -1) {
-        perror("accept");
-        close(server_fd);
-        return;
+    struct Metrics *metrics = (struct Metrics *) cls;
+
+    if (strcmp(method, "GET") != 0) {
+        return MHD_NO;
     }
 
-    char buffer[1024] = {0};
+    char response_buffer[512] = {0};
+    int response_code = MHD_HTTP_OK;
 
-    read(client_fd, buffer, sizeof(buffer) - 1);
-
-    const char *response_tpl =
-        "HTTP/1.1 %s\r\n"
-        "Content-Type: text/plain\r\n"
-        "Content-Length: %d\r\n"
-        "Connection: close\r\n"
-        "\r\n"
-        "%s";
-
-    if (strncmp(buffer, "GET /metrics ", strlen("GET /metrics ")) == 0) {
+    if (strcmp(url, "/metrics") == 0) {
         LOG("Metrics server: GET /metrics");
 
         const char *metrics_tpl =
             "success_total %d\n"
             "failures_total %d\n";
 
-        char metrics_buffer[128] = {0};
         snprintf(
-            metrics_buffer,
-            sizeof(metrics_buffer) - 1,
+            response_buffer,
+            sizeof(response_buffer) - 1,
             metrics_tpl,
             metrics->success_total,
             metrics->failure_total
         );
-
-        snprintf(
-            buffer,
-            sizeof(buffer) - 1,
-            response_tpl,
-            "200 OK",
-            strlen(metrics_buffer),
-            metrics_buffer
-        );
-    } else if (strncmp(buffer, "GET / ", strlen("GET / ")) == 0) {
+        response_code = MHD_HTTP_OK;
+    } else if (strcmp(url, "/") == 0) {
         LOG("Metrics server: GET /");
 
-        const char *body = "See /metrics.";
-        snprintf(
-            buffer,
-            sizeof(buffer) - 1,
-            response_tpl,
-            "200 OK",
-            strlen(body),
-            body
-        );
+        strncpy(response_buffer, "See /metrics.", sizeof(response_buffer) - 1);
+        response_code = MHD_HTTP_OK;
     } else {
-        LOG("Metrics server: Unexpected request");
+        LOG("Metrics server: GET {TODO: url}");
 
-        const char *body = "Not found.";
-        snprintf(
-            buffer,
-            sizeof(buffer) - 1,
-            response_tpl,
-            "404 Not Found",
-            strlen(body),
-            body
-        );
+        strncpy(response_buffer, "Not found", sizeof(response_buffer) - 1);
+        response_code = MHD_HTTP_NOT_FOUND;
     }
 
-    write(client_fd, buffer, strlen(buffer));
+    struct MHD_Response *response = MHD_create_response_from_buffer(
+        strlen(response_buffer),
+        (void *) response_buffer,
+        MHD_RESPMEM_MUST_COPY
+    );
+    enum MHD_Result ret = MHD_queue_response(connection, response_code, response);
+    MHD_destroy_response(response);
 
-    close(client_fd);
-}
-
-
-void run_metrics_server(const char *host, int port, struct Metrics *metrics, int exit_pipe_fd)
-{
-    LOG("Start metrics server at %s:%d...", host, port);
-
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
-        return;
-    }
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-        perror("setsockopt");
-        close(server_fd);
-        return;
-    }
-    int flags = fcntl(server_fd, F_GETFL, 0);
-    if (flags == -1) {
-        perror("fcntl (get)");
-        close(server_fd);
-        return;
-    }
-    if (fcntl(server_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-        perror("fcntl (set)");
-        close(server_fd);
-        return;
-    }
-
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(host);
-    address.sin_port = htons(port);
-
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
-        perror("bind failed");
-        close(server_fd);
-        return;
-    }
-    if (listen(server_fd, 1) == -1) {
-        perror("listen");
-        close(server_fd);
-        return;
-    }
-
-    struct pollfd poll_fds[2];
-
-    poll_fds[0].fd = server_fd;
-    poll_fds[0].events = POLLIN;
-
-    poll_fds[1].fd = exit_pipe_fd;
-    poll_fds[1].events = POLLIN;
-
-    LOG("Metrics server: listening...");
-
-    while (1) {
-        if (poll(poll_fds, 2, -1) == -1) {
-            perror("poll");
-            break;
-        }
-
-        if (poll_fds[1].revents & POLLIN) {
-            break;
-        }
-
-        if (poll_fds[0].revents & POLLIN) {
-            handle_http_request(server_fd, address, metrics);
-        }
-    }
-
-    close(server_fd);
-
-    LOG("Stop metrics server.");
-}
-
-
-void *metrics_server_handler(void *raw_args)
-{
-    struct MetricsThreadArgs *args = (struct MetricsThreadArgs *) raw_args;
-    run_metrics_server(args->host, args->port, args->metrics, args->exit_pipe_fd);
-    return NULL;
+    return ret;
 }
 
 
@@ -324,28 +214,20 @@ int main(int argc, char *argv[])
         }
     }
 
-    // Pipe to gracefully terminate metrics thread.
-    int metrics_thread_pipe_fd[2];
-
-    pthread_t metrics_thread;
+    struct MHD_Daemon *daemon = NULL;
 
     if (metrics_host != NULL) {
-        if (pipe(metrics_thread_pipe_fd) == -1) {
-            perror("pipe");
-            return 1;
-        }
-
-        struct MetricsThreadArgs args;
-        args.host = metrics_host;
-        args.port = metrics_port;
-        args.metrics = &metrics;
-        args.exit_pipe_fd = metrics_thread_pipe_fd[0];
-
-        if (pthread_create(&metrics_thread, NULL, metrics_server_handler, &args) != 0) {
-            perror("pthread_create metrics_thread");
-            close(metrics_thread_pipe_fd[0]);
-            close(metrics_thread_pipe_fd[1]);
-
+        daemon = MHD_start_daemon(
+            MHD_USE_SELECT_INTERNALLY,
+            metrics_port,  // TODO: metrics_host ?
+            NULL,
+            NULL,
+            &handle_http_request,
+            &metrics,
+            MHD_OPTION_END
+        );
+        if (daemon == NULL) {
+            fprintf(stderr, "Failed to start server\n");
             return 1;
         }
     }
@@ -353,12 +235,7 @@ int main(int argc, char *argv[])
     int exit_code = run_command(&argv[optind], repeat, retries_on_failure, &metrics);
 
     if (metrics_host != NULL) {
-        write(metrics_thread_pipe_fd[1], "x", 1);
-
-        pthread_join(metrics_thread, NULL);
-
-        close(metrics_thread_pipe_fd[0]);
-        close(metrics_thread_pipe_fd[1]);
+        MHD_stop_daemon(daemon);
     }
 
     return exit_code;
